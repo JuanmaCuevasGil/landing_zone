06c27774a2e3263095584b19de913af5247c1cce
jacobolf <jlemafurelos@ESWCND2210Q86.es.deloitte.com>
2024-06-05 17:06:11 +0200

    pull de comentarios 2.0

--- modules/EC2/ec2/locals.tf
+++ modules/EC2/ec2/locals.tf
@@ -4,7 +4,7 @@ locals {
   #!/bin/bash
   sudo su -
 
-  # Install apache2 web server, enable and start the service
+  # Installation of apache2 web server, then it enables and starts the service
   apt update -y
   apt install apache2 -y
   systemctl enable apache2
@@ -20,10 +20,14 @@ locals {
     mysql      = <<-EOF
   #!/bin/bash
   sudo su -
+  
+  # Installation of mysql server, then it enables and starts the service
   apt update -y
   apt install mysql-server
   systemctl start mysql
   systemctl enable mysql
+  
+  # Installation of the firewall and application of the rules
   apt install firewalld -y
   firewall-cmd --zone=public --change-interface=eth0 --permanent
   firewall-cmd --zone=public --add-port=3306/tcp --permanent
@@ -32,8 +36,12 @@ locals {
     monitoring = <<-EOF
   #!/bin/bash
   sudo su -
+
+  # Installation of the aws to access the s3 bucket
   apt update -y
   apt install awscli -y
+
+  # Installation of the firewall and application of the rules
   apt install firewalld -y
   firewall-cmd --zone=private --change-interface=eth0 --permanent
   firewall-cmd --zone=public --add-service=ssh --permanent
@@ -42,21 +50,36 @@ locals {
     jumpserver = <<-EOF
   #!/bin/bash
   sudo su -
+
+  # Creation of the keys directory
   mkdir /.ssh
+
+  # Insertion of the keys in the directory, while applying the correct permissions and changing the owner
   echo "${var.key_pair_pem["private"].private_key_pem}" > /.ssh/${var.keys.key_name["private"]}.pem
   chmod 400 /.ssh/${var.keys.key_name["private"]}.pem
   echo "${var.key_pair_pem["vpn"].private_key_pem}" > /.ssh/${var.keys.key_name["vpn"]}.pem
   chmod 400 /.ssh/${var.keys.key_name["vpn"]}.pem
   chown -R ubuntu /.ssh
+
+  # We add the VPN server IP to known hosts, we need this to auto confirm the scp command
   ssh-keyscan -H ${var.vpn_ip} | sudo tee -a ~/.ssh/known_hosts
+  
+  
+  # Installation of the firewall and OpenVPN
   apt update -y
   apt install firewalld -y
   apt install openvpn -y
+
+  # Application of the firewall rules
   firewall-cmd --zone=public --change-interface=eth0 --permanent
   firewall-cmd --zone=public --add-service=ssh --permanent
   firewall-cmd --zone=public --add-service=openvpn --permanent
   firewall-cmd --reload
+
+  # Import of the client configuration file from the VPN server
   scp -i /.ssh/${var.keys.key_name["vpn"]}.pem ubuntu@${var.vpn_ip}:/home/ubuntu/jumpserver.ovpn /home/ubuntu/
+  
+  # Application of the client configuration file
   openvpn --config /home/ubuntu/jumpserver.ovpn
   EOF
    vpn = <<-EOF
--- modules/EC2/ec2/vpn/locals.tf
+++ modules/EC2/ec2/vpn/locals.tf
@@ -2,6 +2,8 @@ locals {
   vpn = <<-EOF
   #!/bin/bash
   sudo su -
+
+  # Installation of the firewall and application of the rules
   apt install firewalld -y
   firewall-cmd --zone=public --change-interface=eth0 --permanent
   firewall-cmd --zone=public --add-service=openvpn --permanent
@@ -11,16 +13,31 @@ locals {
   firewall-cmd --zone=public --add-port=22/udp --permanent
   firewall-cmd --zone=public --add-port=22/tcp --permanent
   firewall-cmd --reload
+
+  # Creation of the keys directory
   mkdir /.ssh
+
+  # Insertion of the key in the directory, while applying the correct permissions and changing the owner
   echo "${var.key_pair_pem["public"].private_key_pem}" > /.ssh/${var.keys.key_name["public"]}.pem
   chmod 400 /.ssh/${var.keys.key_name["public"]}.pem
   chown -R ubuntu /.ssh
+  
+  # We import an OpenVPN installer and apply execution permission
   curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
   chmod +x openvpn-install.sh
+
+  # We set the correct parameters to make an automatic installation for the VPN server
+  # Set the public IP address as endoint
   export ENDPOINT=$(curl http://checkip.amazonaws.com)
+  # Set the client name
   export CLIENT=jumpserver
+  # Enable the auto installation
   export AUTO_INSTALL=y
+
+  # Execute the script
   ./openvpn-install.sh
+
+  # We copy the generated file to an accesible directory, while changing file's ownership
   cp /root/jumpserver.ovpn /home/ubuntu
   chown ubuntu:ubuntu /home/ubuntu/jumpserver.ovpn
   EOF
